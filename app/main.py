dad93bb65e70bb61cd76b57896f8c4c4403bed9e
Creampanda <andrewfromtokyo@gmail.com>
2022-07-05 10:50:47 +0300

    Used Class instead of func and speed up parsing

--- app/main.py
+++ app/main.py
@@ -1,14 +1,15 @@
 from app import db
-from parsing_functions import parse_all
-from config import API_URL
+from parsing_functions import Parser
 from psycopg2.extras import NamedTupleCursor, DictCursor
 
 
-def parse_latest_and_save(start_area, end_area, i, j):
+def parse_latest_and_save(proxies, start, end, search_period):
+    # print(proxies, start, end, search_period)
+    # print(type(proxies), type(start), type(end), type(search_period))
+    
+    parser = Parser(proxies, start, end, search_period)
     with db, db.cursor(cursor_factory=NamedTupleCursor) as cur:
-        for vacancy_dict in parse_all(
-            API_URL, latest_only=False, start_area=start_area, end_area=end_area, i=i, j=j
-        ):
+        for vacancy_dict in parser.parse_all():
             cur.execute(
                 """
                 INSERT INTO vacancies
@@ -28,4 +29,13 @@ def parse_latest_and_save(start_area, end_area, i, j):
             cur.execute("COMMIT")
 
 
-parse_latest_and_save(start_area=10, end_area=-1, i=0, j=-1)
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument("proxies", type=str, nargs="+", help="")
+    parser.add_argument("-start", type=int, help="", default=0)
+    parser.add_argument("-end", type=int, help="", default=-1)
+    parser.add_argument("-search_period", type=int, help="", default=0)
+    args = parser.parse_args()
+    parse_latest_and_save(**vars(args))
--- app/parsing_functions.py
+++ app/parsing_functions.py
@@ -6,242 +6,224 @@ from random import random, choice
 from config import *
 from proxy_list import proxy_list
 
-session = requests.Session()
-user = fake_useragent.UserAgent().random
-header = {"user_agent": user}
 
+class Parser:
+    def __init__(self, proxies, start_area=0, end_area=-1, search_period=0) -> None:
+        self.session = requests.Session()
+        user = fake_useragent.UserAgent().random
+        self.user = user
+        self.header = {"user_agent": user}
+        self.url = API_URL
+        self.proxies = proxies
+        self.areas = AREA_IDS[start_area:end_area]
+        self.search_period = search_period
+        print(f"target areas: {self.areas[0]} ... {self.areas[-1]}")
 
-def set_proxy(i=0, j=-1):
-    global session
-    proxy = choice(proxy_list[i:j])
-    print(f"{proxy} - proxylist")
-    session.proxies = {
-        "http": f"http://{proxy}",
-        "https": f"http://{proxy}",
-    }
-    resp = session.get("https://httpbin.org/ip")
-    print(resp.text)
-
-
-def parse_developers():
-    params = {"area": 1, "professional_role": 96}
-    for industry in [7.538, 7.539, 7.540, 7.541]:
-        params["industry"] = industry
-        for exp in EXPERIENCE:
-            for exp in EXPERIENCE:
-                # print(
-                #     f"industry_id = {industry},\
-                #     experience = {exp}"
-                # )
-                params["experience"] = exp
-                resp = session.get(API_URL, headers=header, params=params)
-                resp.encoding = "utf-8"
-                data = json.loads(resp.text)
-                print(f"Found {data['found']} vacancies for ", params)
-                if data["found"] == 0:
-                    continue
-                for vacancy in get_and_save_by_params(API_URL, params=params):
-                    yield vacancy
-
-
-def get_and_save_by_params(url, params):
-    resp = session.get(url, headers=header, params=params)
-    resp.encoding = "utf-8"
-    data = json.loads(resp.text)
-    if not data.get("pages") or not data.get("items"):
+    def set_proxy(self):
+        proxy = choice(self.proxies)
+        self.session.proxies = {
+            "http": f"http://{proxy}",
+            "https": f"http://{proxy}",
+        }
+        resp = self.session.get("https://httpbin.org/ip")
         print("========================================")
-        print(data.keys())
+        print("Connected via: " + resp.json()["origin"])
         print("========================================")
-        resp = session.get(url, headers=header, params=params)
+
+    def get_response_json(self, params):
+        resp = self.session.get(self.url, headers=self.header, params=params)
+        assert resp.status_code == 200
         resp.encoding = "utf-8"
-        data = json.loads(resp.text)
+        return json.loads(resp.text)
 
-    total_pages = data["pages"]
-    for i in range(0, total_pages):
-        sleep(random() * 5)
-        params["page"] = i
-        resp = session.get(url, headers=header, params=params)
-        resp.encoding = "utf-8"
-        data = json.loads(resp.text)
-        for vacancy in data["items"]:
-            try:
-                vacancy_name = vacancy["name"]
-            except:
-                vacancy_name = None
-            try:
-                area = vacancy["area"]["name"]
-            except:
-                area = None
-            try:
-                employer = vacancy["employer"]["name"]
-            except:
-                employer = None
-            try:
-                salary_from = vacancy["salary"]["from"]
-            except:
-                salary_from = None
-            try:
-                salary_to = vacancy["salary"]["to"]
-            except:
-                salary_to = None
-            try:
-                currency = vacancy["salary"]["currency"]
-            except:
-                currency = None
-            try:
-                published_at = vacancy["published_at"]
-            except:
-                published_at = None
-            try:
-                created_at = vacancy["created_at"]
-            except:
-                created_at = None
-            try:
-                archived = vacancy["archived"]
-                if archived is False:
-                    archived = None
-            except:
-                archived = None
-            try:
-                schedule = vacancy["schedule"]["name"]
-            except:
-                schedule = None
+    def get_and_save_by_params(self, params):
+        data = self.get_response_json(params)
+        if not data.get("pages") or not data.get("items"):
+            print("========================================")
+            print(data.keys())
+            print("========================================")
+            data = self.get_response_json(params)
 
-            vacancy_url = vacancy["alternate_url"]
-            vacancy_id = vacancy_url.split("/")[-1]
+        total_pages = data["pages"]
+        for i in range(total_pages):
+            sleep(random() * 5)
+            params["page"] = i
+            if i > 0:
+                data = self.get_response_json(params)
 
-            vacancy_dict = {
-                "vacancy_id": vacancy_id,
-                "vacancy_name": vacancy_name,
-                "profession": PROFESSIONAL_ROLES_ALL[params["professional_role"]],
-                "industry": INDUSTRIES[params["industry"]],
-                "experience": params["experience"],
-                "key_skills": None,
-                "employer": employer,
-                "area": area,
-                "salary_from": salary_from,
-                "salary_to": salary_to,
-                "currency": currency,
-                "job_description": None,
-                "published_at": published_at,
-                "created_at": created_at,
-                "archived": archived,
-                "employment_type": schedule,
-                "vacancy_url": vacancy_url,
-            }
-            yield vacancy_dict
-
-
-def parse_all(url, latest_only=False, start_area=None, end_area=None, i=0, j=-1):
-    set_proxy(i, j)
-
-    target_areas = AREA_IDS
-    if start_area:
-        target_areas = target_areas[start_area:]
-    if end_area:
-        target_areas = target_areas[:end_area]
-    print(f"target areas: {target_areas[0]} ... {target_areas[-1]}")
-    for area in target_areas:
-
-        params = dict()
-        if latest_only:
-            params["search_period"] = 1
-        sleep(random() * 5)
-        params["area"] = area
-        try:
-            resp = session.get(url, headers=header, params=params)
-            assert resp.status_code == 200
-        except:
-            set_proxy(i, j)
-            resp = session.get(url, headers=header, params=params)
-            assert resp.status_code == 200
-        resp.encoding = "utf-8"
-        data = json.loads(resp.text)
-        # print(data.keys())
-        # print(f"area={area}, found={data['found']}")
-        print(f"Found {data['found']} vacancies for ", params)
-        if data["found"] == 0:
-            continue
-        else:
-            for professional_role_id in PROFESSIONAL_ROLES_ALL.keys():
-                #
-                # REMOVE
-                #
-                if area == 2 and professional_role_id < 8:
-                    continue
-                #
-                # REMOVE
-                #
-                sleep(random() * 5)
-                params["professional_role"] = professional_role_id
-                if params.get("industry"):
-                    params.pop("industry")
-                if params.get("experience"):
-                    params.pop("experience")
+            for vacancy in data["items"]:
                 try:
-                    resp = session.get(url, headers=header, params=params)
-                    assert resp.status_code == 200
+                    vacancy_name = vacancy["name"]
                 except:
-                    set_proxy(i, j)
-                    resp = session.get(url, headers=header, params=params)
-                    assert resp.status_code == 200
-                resp.encoding = "utf-8"
-                data = json.loads(resp.text)
-                print(f"Found {data['found']} vacancies for ", params)
-                if data["found"] == 0:
-                    continue
-                else:
-                    for industry_id in INDUSTRIES.keys():
-                        sleep(random() * 5)
-                        if (
-                            area == 1
-                            and professional_role_id == 96
-                            and industry_id == 7
-                            and not latest_only
-                        ):
-                            continue
-                        # print(
-                        #     f"area={area}, profession_id = {professional_role_id}, industry_id = {industry_id}"
-                        # )
-                        params["industry"] = industry_id
-                        if params.get("experience"):
-                            params.pop("experience")
-                        try:
-                            resp = session.get(url, headers=header, params=params)
-                            assert resp.status_code == 200
-                        except:
-                            set_proxy(i, j)
-                            resp = session.get(url, headers=header, params=params)
-                            assert resp.status_code == 200
-                        resp.encoding = "utf-8"
-                        data = json.loads(resp.text)
-                        print(f"Found {data['found']} vacancies for ", params)
-                        if data["found"] == 0:
-                            continue
-                        else:
-                            for exp in EXPERIENCE:
-                                sleep(random() * 5)
-                                # print(
-                                #     f"area={area}, profession_id = {professional_role_id}, industry_id = {industry_id}, experience = {exp}"
-                                # )
-                                params["experience"] = exp
-                                try:
-                                    resp = session.get(
-                                        url, headers=header, params=params
-                                    )
-                                    assert resp.status_code == 200
-                                except:
-                                    set_proxy(i, j)
-                                    resp = session.get(
-                                        url, headers=header, params=params
-                                    )
-                                    assert resp.status_code == 200
-                                resp.encoding = "utf-8"
-                                data = json.loads(resp.text)
-                                print(f"Found {data['found']} vacancies for ", params)
-                                if data["found"] == 0:
-                                    continue
-                                for vacancy_dict in get_and_save_by_params(
-                                    url, params=params
+                    vacancy_name = None
+                try:
+                    area = vacancy["area"]["name"]
+                except:
+                    area = None
+                try:
+                    employer = vacancy["employer"]["name"]
+                except:
+                    employer = None
+                try:
+                    salary_from = vacancy["salary"]["from"]
+                except:
+                    salary_from = None
+                try:
+                    salary_to = vacancy["salary"]["to"]
+                except:
+                    salary_to = None
+                try:
+                    currency = vacancy["salary"]["currency"]
+                except:
+                    currency = None
+                try:
+                    published_at = vacancy["published_at"]
+                except:
+                    published_at = None
+                try:
+                    created_at = vacancy["created_at"]
+                except:
+                    created_at = None
+                try:
+                    archived = vacancy["archived"]
+                    if archived is False:
+                        archived = None
+                except:
+                    archived = None
+                try:
+                    schedule = vacancy["schedule"]["name"]
+                except:
+                    schedule = None
+                try:
+                    profession = PROFESSIONAL_ROLES_ALL[params["professional_role"]]
+                except:
+                    profession = None
+                try:
+                    INDUSTRIES[params["industry"]]
+                except:
+                    industry = None
+                try:
+                    experience = params["experience"]
+                except:
+                    experience = None
+
+                vacancy_url = vacancy["alternate_url"]
+                vacancy_id = vacancy_url.split("/")[-1]
+
+                vacancy_dict = {
+                    "vacancy_id": vacancy_id,
+                    "vacancy_name": vacancy_name,
+                    "profession": profession,
+                    "industry": industry,
+                    "experience": experience,
+                    "key_skills": None,
+                    "employer": employer,
+                    "area": area,
+                    "salary_from": salary_from,
+                    "salary_to": salary_to,
+                    "currency": currency,
+                    "job_description": None,
+                    "published_at": published_at,
+                    "created_at": created_at,
+                    "archived": archived,
+                    "employment_type": schedule,
+                    "vacancy_url": vacancy_url,
+                }
+                yield vacancy_dict
+
+    def parse_all(self):
+        self.set_proxy()
+        for area in self.areas:
+            params = dict()
+            params["search_period"] = self.search_period
+            sleep(random() * 5)
+            params["area"] = area
+            params["per_page"] = 100
+            try:
+                data = self.get_response_json(params)
+            except:
+                self.set_proxy()
+                data = self.get_response_json(params)
+            print(f"Found {data['found']} vacancies for ", params)
+            if data["found"] == 0:
+                continue
+            if data["found"] <= 2000:
+                for vacancy_dict in self.get_and_save_by_params(params=params):
+                    yield vacancy_dict
+            else:
+                for professional_role_id in PROFESSIONAL_ROLES_ALL.keys():
+                    sleep(random() * 5)
+                    params["professional_role"] = professional_role_id
+                    if params.get("industry"):
+                        params.pop("industry")
+                    if params.get("experience"):
+                        params.pop("experience")
+                    try:
+                        data = self.get_response_json(params)
+                    except:
+                        self.set_proxy()
+                        data = self.get_response_json(params)
+                    print(f"Found {data['found']} vacancies for ", params)
+                    if data["found"] == 0:
+                        continue
+                    if data["found"] <= 2000:
+                        for vacancy_dict in self.get_and_save_by_params(params=params):
+                            yield vacancy_dict
+                    else:
+                        for industry_id in INDUSTRIES.keys():
+                            sleep(random() * 5)
+                            params["industry"] = industry_id
+                            if params.get("experience"):
+                                params.pop("experience")
+                            try:
+                                data = self.get_response_json(params)
+                            except:
+                                self.set_proxy()
+                                data = self.get_response_json(params)
+                            print(f"Found {data['found']} vacancies for ", params)
+                            if data["found"] == 0:
+                                continue
+                            if data["found"] <= 2000:
+                                for vacancy_dict in self.get_and_save_by_params(
+                                    params=params
                                 ):
                                     yield vacancy_dict
+                            else:
+                                for exp in EXPERIENCE:
+                                    sleep(random() * 5)
+                                    params["experience"] = exp
+                                    try:
+                                        data = self.get_response_json(params)
+                                    except:
+                                        self.set_proxy()
+                                        data = self.get_response_json(params)
+                                    print(
+                                        f"Found {data['found']} vacancies for ", params
+                                    )
+                                    if data["found"] == 0:
+                                        continue
+                                    for vacancy_dict in self.get_and_save_by_params(
+                                        params=params
+                                    ):
+                                        yield vacancy_dict
+
+
+# def parse_developers():
+#     params = {"area": 1, "professional_role": 96}
+#     for industry in [7.538, 7.539, 7.540, 7.541]:
+#         params["industry"] = industry
+#         for exp in EXPERIENCE:
+#             for exp in EXPERIENCE:
+#                 # print(
+#                 #     f"industry_id = {industry},\
+#                 #     experience = {exp}"
+#                 # )
+#                 params["experience"] = exp
+#                 resp = session.get(API_URL, headers=header, params=params)
+#                 resp.encoding = "utf-8"
+#                 data = json.loads(resp.text)
+#                 print(f"Found {data['found']} vacancies for ", params)
+#                 if data["found"] == 0:
+#                     continue
+#                 for vacancy in get_and_save_by_params(API_URL, params=params):
+#                     yield vacancy
